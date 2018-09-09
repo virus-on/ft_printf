1d822eba1ca65c9e6c6d57005f33788ca6b17d17
Maksym Paziuk <mpaziuk@e1r5p13.unit.ua>
2018-09-09 13:02:58 +0300

    error?

--- src/ft_wide_str.c
+++ src/ft_wide_str.c
@@ -2,7 +2,8 @@
 
 void	ft_wstr_put(wchar_t *str, int len, t_numbers *n)
 {
-	n->return_i += write(STD_OUT, str, len) + 5;
+	write(STD_OUT, str, len - 1)
+	n->return_i +=  len - 1;
 }
 
 int		ft_wstrlen(const wchar_t *s)
